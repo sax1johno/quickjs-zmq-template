a53b107097653b6d3c426f3d7f4502941e20763c
John O'Connor <sax1johno@gmail.com>
2022-03-16 13:47:56 -0700

    Got the state machine running

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { SimulatedClock } from 'xstate/lib/SimulatedClock'
 import { runtimeMachine} from "./simpleStateMachine";
 import {setTimeout, clearTimeout} from "os";
 
+// Use the setTimeout and clearTimeout from the OS package since it's not included.
 const testService = interpret(runtimeMachine, {
     clock: {
         setTimeout: setTimeout,
